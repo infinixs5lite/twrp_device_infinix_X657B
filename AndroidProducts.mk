b9aed6c8b8bb04f61f91bcdac85e757541add708
infinixs5lite <kelvinchinedu97@gmail.com>
2023-03-07 10:40:26 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_Infinix-X657B.mk
+    $(LOCAL_DIR)/omni_X657B.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_Infinix-X657B-user \
-    omni_Infinix-X657B-userdebug \
-    omni_Infinix-X657B-eng
+    omni_X657B-eng
